57cbd3970ff0e15fce34dd0d0c223f17c45c9744
Seyed Ali Mohseni <71790490+mohseni1983@users.noreply.github.com>
2023-01-02 13:51:57 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
 //require('dotenv').config();
+const path=require('path')
+const fs=require('fs');
 const express=require('express');
 const mysql=require('mysql')
 const app=express()
+
 const connector=mysql.createConnection({
-    host: 'localhost',
-    database: 'asterisk',
-    user: 'root',
-    password: '20242170',
-    port: 3306
-})
-
-app.use('/', (req,res)=>{
-    res.set('Content-Type','text/xml')
-    const result=new Promise((resolve,reject)=>{
-        connector.query( "SELECT description AS Name, user AS Telephone FROM devices WHERE tech='sip'",
-            (error,response)=>{
-                if(error){
-                    reject(error)
-                }else{
-                    let xmlData="<YealinkIPPhoneDirectory>"
-                    xmlData+="<DirectoryEntry>"
-                    for(let item of response){
-                        if(!isNaN(item.Name))
-                            {item.Name = 'Ext-'+item.Name}
-                        xmlData+=`<${item.Name.replace(' ','_')}>${item.Telephone}</${item.Name.replace(' ','_')}>`
+     host: 'localhost',
+     database: 'asterisk',
+     user: 'root',
+     password: 'elyas@1401',
+     port: 3306
+ })
 
 
-		        }
-                    xmlData+="</DirectoryEntry>"
-                    xmlData+="</YealinkIPPhoneDirectory>"
-                    resolve(xmlData)
-                }
+app.use('/data',(req,res,next)=>{
+    console.log('File requested');
+    connector.query( "SELECT description AS Name, user AS Telephone FROM devices WHERE tech='sip'",
+    (error,response)=>{
+        if(error){
+            console.log(error)
+        }else{
+            let xmlData="<YealinkIPPhoneDirectory>"
+            
+            for(let item of response){
+                xmlData+="<DirectoryEntry>"
+                if(!isNaN(item.Name))
+                    item.Name='Ext-'+item.Name
+                xmlData+=`<Name>${item.Name}</Name>`
+                xmlData+=`<Telephone>${item.Telephone}</Telephone>`
+                xmlData+="</DirectoryEntry>"
             }
-        )
-    })
-    result.then(dt=>{
-        res.send(dt)
-    }).catch(err=>{
-        throw err
-    })
+            
+            xmlData+="</YealinkIPPhoneDirectory>"
+            fs.writeFile(path.join(__dirname,'data','contacts.xml'),xmlData,(er)=>{
+                if(er){
+                    console.log(er)
+                }else{
+                    console.log('File created');
+                    next()
+                }
+            })
+
+        }
+    }
+)
+    
 })
+app.use('/data',express.static(path.join(__dirname,'data')))
+
+
 app.listen(4000,()=>{
     console.log('Server started at port 4000')
 })
